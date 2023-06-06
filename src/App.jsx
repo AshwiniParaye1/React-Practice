5f7485736dd05371514c75a0145766efb9b1a281
Ashwini <ashwiniparaye9309@gmail.com>
2023-06-06 16:53:37 +0530

    map

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import img from "./assets/glass1.jpg";
+import { useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [cardData, setCardData] = useState([]);
+
+  useEffect(() => {
+    const data = async () => {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+
+      // console.log("response ==", response);
+      setCardData(response.data);
+    };
+
+    data();
+  }, []);
+
+  console.log("card data ==", cardData);
+
   return (
     <div
       className="container"
       style={{
         display: "flex",
-        flexDirection: "column",
+        flexDirection: "row",
+        gap: "10px",
         justifyContent: "center",
         alignItems: "center",
-        height: "100vh",
+        maxHeight: "764px",
         textAlign: "center",
+        flexWrap: "wrap",
       }}
     >
-      <div
-        className="card"
-        style={{
-          width: "400px",
-          border: "1px  solid black",
-          padding: "16px",
-          borderRadius: "4px",
-        }}
-      >
-        <h1>Card</h1>
-        <img
-          src={img}
-          alt=""
-          style={{
-            width: "300px",
-            height: "200px",
-            borderRadius: "5px",
-          }}
-        />
+      {cardData.map((newData) => (
         <div
-          className="card-content"
-          style={{ display: "flex", padding: "16px", flexDirection: "column" }}
+          key={newData.id}
+          className="card"
+          style={{
+            width: "400px",
+            height: "400px",
+            border: "1px  solid black",
+            padding: "5px",
+            borderRadius: "4px",
+            overflow: "hidden",
+          }}
         >
-          <h1>Main Card content</h1>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum
-            nesciunt, odio dolorem totam itaque voluptatem qui ullam. Obcaecati
-            corporis nisi quod, ipsam quam id harum illum temporibus nulla
-            impedit enim!
-          </p>
+          <h4>{newData.title}</h4>
+
+          <img
+            src={img}
+            alt=""
+            style={{
+              width: "300px",
+              height: "200px",
+              borderRadius: "5px",
+            }}
+          />
+          <div
+            className="card-content"
+            style={{
+              display: "flex",
+              padding: "5px",
+              flexDirection: "column",
+            }}
+          >
+            <h4>User id: {newData.userId}</h4>
+            <p>{newData.body}</p>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
